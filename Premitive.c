05fec4b71214b04ca670ff3d1717b79460d88b53
Aadi <aaditishinde1804@gmail.com>
2024-08-15 22:34:21 +0530

    sizeOf

---
+++ Premitive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch = 'A';
+
+    int no = 10;
+
+    float f = 89.12;
+
+    double d = 86.897654;
+
+    printf("%c\n" , ch);
+    printf("%d\n" , no);
+    printf("%f\n" , f);
+    printf("%lf\n" , d);
+
+
+    return 0;
+}
