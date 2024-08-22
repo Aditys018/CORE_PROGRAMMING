c8458ab365863969b984be3a70c64287198dcb75
Aadi <aaditishinde1804@gmail.com>
2024-08-22 20:01:14 +0530

    Arrays1

---
+++ Array1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    float marks1 = 78.98;
+    float marks2 = 90.99;
+    float marks3 = 67.89;
+    float marks4 = 56.90;
+    float marks5 = 87.56;
+
+    //Marks is a one dimentional array which contains 5 elements,
+    //each element is of type float
+    float Marks[5] = {78.98,90.99,67.89,56.90,87.56};
+
+
+
+    return 0;
+}
