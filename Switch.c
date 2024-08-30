5b4b644b98a3392bab6628c3810cba83575e6068
Aadi <aaditishinde1804@gmail.com>
2024-08-30 21:19:45 +0530

    ForWhile

--- Switch.c
+++ Switch.c
@@ -1,8 +1,8 @@
+
 #include<stdio.h>
 
 int main()
 {
-
     int Standard = 0;
 
     printf("Enter your standard : \n");
@@ -13,25 +13,22 @@ int main()
         case 1:
             printf("Your exam is at 8 AM\n");
             break;
-
-        case2:
+        
+        case 2:
             printf("Your exam is at 9 AM\n");
             break;
-
-        case3:
+                    
+        case 3:
             printf("Your exam is at 10 AM\n");
             break;
-
+        
         case 4:
             printf("Your exam is at 11 AM\n");
-            break;
+            break;  
 
         default:
-            printf("Wrong choice\n");
-
+            printf("You entered wrong choice\n");  
     }
-
-
 
     return 0;
 }
