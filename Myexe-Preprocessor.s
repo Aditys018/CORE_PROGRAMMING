08d2e77cb66f709abb564c006d1ace0e2f2f23c5
Aadi <aaditishinde1804@gmail.com>
2024-08-30 22:54:25 +0530

    Preprocessor

---
+++ Myexe-Preprocessor.s
@@ -0,0 +1,29 @@
+	.file	"Preprocessor.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello..."
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$12, -4(%rbp)
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 14.2.1 20240801 (Red Hat 14.2.1-1)"
+	.section	.note.GNU-stack,"",@progbits
---
+++ Preprocessor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define DOZEN 12
+
+int main()     //Entrypoint function
+{
+    int no = DOZEN;
+
+    printf("Hello...\n");
+
+    return 0;
+}
+
+// gcc Preprocessor.c -o Myexe -save-temps
+//from the above command we can get .i, .c. etc...files. 
