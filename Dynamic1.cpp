50a3befc094ed89b7e7afec698f32e109b1a3569
Aadi <aaditishinde1804@gmail.com>
2024-09-20 22:34:25 +0530

    Amend

--- Dynamic1.cpp
+++ Dynamic1.cpp
@@ -8,12 +8,13 @@ int main()
 
     //step1 allocate the memory
     ptr = new int [5];
+    //ptr = (int*)malloc(5*sizeof(int));  //similar syntax in c
 
     //step2 use the memory
     //logic
 
     //step3 Deallocate the memory
     delete []ptr;
-    
+
     return 0;
 }
