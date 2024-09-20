f7921bc618343997ae922e2dda9ae64dff730c9e
Aadi <aaditishinde1804@gmail.com>
2024-09-20 22:43:40 +0530

    Add Generic.c and this.cpp

---
+++ this.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+    int No1, No2;
+
+    Demo(int A, int B)
+    {
+        No1 = A;
+        No2 = B;
+    }
+    void Display(int X) // display naavachi method, 
+    {
+        cout<<"Value of No1 : "<<No1<<"\n";
+        cout<<"Value of No2 : "<<No2<<"\n";
+        cout<<"value of X : "<<X<<"\n";
+    }
+
+};
+int main()
+{
+    Demo obj(11,21);
+    obj.Display(51);
+
+    return 0;
+}
