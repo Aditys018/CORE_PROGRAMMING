1ab1d6e7f2e51592f8fa40a7981a5f9e81d83a97
Aadi <aaditishinde1804@gmail.com>
2024-09-26 20:03:58 +0530

    Redefinition

---
+++ Redef.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        void Fun()    //Definition
+        {
+            cout<<"Inside base Fun\n";
+        }
+};
+
+class Derived : public Base
+{
+    public : 
+        void Fun()
+        {
+            cout<<"Inside Derived Gun\n";
+        }
+};
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";       //1 byte
+    Derived dobj;
+    dobj.Fun();
+
+    Derived bobj;
+    bobj.Fun();
+
+    return 0;
+}
