f0d6c9a8969f06952acf3e9eb9113b2d69aabf07
Aadi <aaditishinde1804@gmail.com>
2024-09-26 22:33:12 +0530

    VIMP,4_FAANG

--- Runtime3.cpp
+++ Runtime3.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Base
 {
     public:
-        int i,j;
-        void Fun()    //These are called Functions
-        {cout<<"Inside base Fun\n";}
-        void Gun()
+        int i,j;    //1000
+        virtual void Fun()    //These are called Functions
+        {cout<<"Inside Base Fun\n";}
+        void Gun()  //2000
         {cout<<"Inside Base Gun\n";}
-        void Sun()
+        virtual void Sun()  //3000
         {cout<<"Inside Base Sun\n";}
-        void Run()
+        virtual void Run()  //4000
         {cout<<"Inside Base Run\n";}
 };
 
@@ -19,19 +19,22 @@ class Derived : public Base
 {
     public : 
         int x,y;
-        void Fun()    //These are called Functions
+        virtual void Fun()  //5000  //These are called Functions
         {cout<<"Inside Derived Fun\n";}
-        void Gun()
-        {cout<<"Inside Base Gun\n";}
-        void Sun()
-        {cout<<"Inside Base Sun\n";}
-        void Mun()
-        {cout<<"Inside Base Mun\n";}
+        void Gun()  //6000
+        {cout<<"Inside Derived Gun\n";}
+        virtual void Mun()  //7000
+        {cout<<"Inside Derived Mun\n";}
+        void Run()  //8000
+        {cout<<"Inside Derived Run\n";}
 };
 
 int main()  //in the main class we have 2 objects and 2 pointers
 {
-    
+    cout<<"Size of base class : "<<sizeof(Base)<<"\n";
+    cout<<"Size of derived class : "<<sizeof(Derived)<<"\n";
+
+
     Derived dobj;   //object
     Base *bptr = NULL;  //pointer
    
